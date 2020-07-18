a50d64efcb80772a224057fe4ba3b193456fa399
achu1998 <43047640+achu1998@users.noreply.github.com>
2020-07-19 00:23:33 +0530

    from

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ function sendEmail() {
                 Username : "ashweenr1998@gmail.com",
                 Password : "ashram1998",
                 To : "crazyideaz1998@gmail.com",
-                From : "crazyideaz1998@gmail.com",
+                From : "ashweenr1998@gmail.com",
                 Subject : "resut link",
                 Body : "link",
             })
